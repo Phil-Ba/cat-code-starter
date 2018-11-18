f249a1fb81b6baa2b19b52373012dcdbcfe9347c
philba <p.bayer@freemail.hu>
2018-11-18 20:58:38 +0100

    bit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,14 @@
+
+K
+src/catcode/MainLevel1.java,e/b/ebb165e55dd4ac9b3cebf41c82c9216faed00d8b
+K
+src/catcode/ArrayUtils.java,7/8/7849f215f5b700fc735d29734df2d48604e46d3f
+D
+src/catcode/test.txt,4/7/47dbc0cbe353e0e3b65e0381c12564906df948e8
+H
+src/catcode/IOUtils.java,8/f/8f7904025cc4379e2e597db02a057ccb04b34767
+L
+src/catcode/NumberUtils.java,d/2/d2a5e6761a91947c76e0f716d72cf6ba4367e364
+:
+
+result.txt,b/d/bdd922bb216a40a75feefa8a8f6b904213bfef80
--- src/catcode/MainLevel1.java
+++ src/catcode/MainLevel1.java
@@ -1,6 +1,6 @@
 package catcode;
 
-import java.util.stream.Stream;
+import java.util.*;
 
 /**
  *
@@ -10,11 +10,69 @@ public class MainLevel1 {
     public static void main(String[] args) {
 //        System.out.println(IOUtils.readFileAsList("test.txt"));
 //        IOUtils.write("out.txt", "1","2","3");
-//        solve1(IOUtils.readFile("level1.txt"));
+        solve1(IOUtils.readFileAsList("level1/level1-1.txt"),"level1/out1-1.txt");
+        solve1(IOUtils.readFileAsList("level1/level1-2.txt"),"level1/out1-2.txt");
+        solve1(IOUtils.readFileAsList("level1/level1-3.txt"),"level1/out1-3.txt");
+        solve1(IOUtils.readFileAsList("level1/level1-4.txt"),"level1/out1-4.txt");
     }
     
-    static void solve1(Stream<String> input){
-    
+    static class Account {
+        int balance;
+        String name;
+        
+        public Account(String name, int balance) {
+            this.balance = balance;
+            this.name = name;
+        }
     }
-
+    
+    static class Transaction {
+        String from, to;
+        int amount;
+        long timestamp;
+        
+        public Transaction(String from, String to, int amount, long timestamp) {
+            this.from = from;
+            this.to = to;
+            this.amount = amount;
+            this.timestamp = timestamp;
+        }
+        
+    }
+    
+    static void solve1(List<String> input, String outloc) {
+        Iterator<String> iter = input.iterator();
+        int accountsNumber = Integer.parseInt(iter.next());
+        HashMap<String, Account> accounts = new HashMap<>();
+        List<Account> accountsList = new ArrayList<>();
+        for (int i = 0; i < accountsNumber; i++) {
+            String[] line = iter.next().split(" ");
+            Account acc = new Account(line[0], Integer.parseInt(line[1]));
+            accounts.put(acc.name, acc);
+            accountsList.add(acc);
+        }
+        
+        int transactionNumber = Integer.parseInt(iter.next());
+        List<Transaction> transactions = new ArrayList<>();
+        for (int i = 0; i < transactionNumber; i++) {
+            String[] line = iter.next().split(" ");
+            Transaction transaction = new Transaction(line[0], line[1], Integer.parseInt(line[2]), Long.parseLong(line[3]));
+            transactions.add(transaction);
+        }
+        
+        transactions.stream()
+                .sorted(Comparator.comparingLong(t -> t.timestamp))
+                .forEach(transaction -> {
+                    Account from = accounts.get(transaction.from);
+                    Account to = accounts.get(transaction.to);
+                    from.balance -= transaction.amount;
+                    to.balance += transaction.amount;
+                });
+        
+        List<String> out = new ArrayList<>();
+        out.add(""+accountsNumber);
+        accountsList.forEach(a -> out.add(a.name + " " + a.balance));
+        IOUtils.write(outloc,out);
+    }
+    
 }
