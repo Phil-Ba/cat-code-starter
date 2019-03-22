663a8c253acab3d2ef67c391cf57d15864794d8c
philba <p.bayer@freemail.hu>
2019-03-22 14:06:09 +0100

    Perp

---
+++ src/catcode/level1/MainLevel1.java
@@ -0,0 +1,22 @@
+package catcode.level1;
+
+import catcode.IOUtils;
+
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+/**
+ *
+ */
+public class MainLevel1 {
+    
+    public static void main(String[] args) {
+        Scanner scanner = IOUtils.scanFile("level1/");
+        solve();
+    }
+    
+    static void solve() {
+    
+    }
+    
+}
