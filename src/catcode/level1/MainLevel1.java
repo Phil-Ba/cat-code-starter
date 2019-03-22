9792ac608e47c44a9fc4448b25098c33a69e7ef8
philba <p.bayer@freemail.hu>
2019-03-22 15:22:33 +0100

    solution 1

--- src/catcode/level1/MainLevel1.java
+++ src/catcode/level1/MainLevel1.java
@@ -3,7 +3,6 @@ package catcode.level1;
 import catcode.IOUtils;
 
 import java.util.Scanner;
-import java.util.stream.Stream;
 
 /**
  *
@@ -11,13 +10,28 @@ import java.util.stream.Stream;
 public class MainLevel1 {
     
     public static void main(String[] args) {
-        Scanner scanner = IOUtils.scanFile("level1/");
-        solve(scanner);
+        for (int i = 1; i < 6; i++) {
+            Scanner scanner = IOUtils.scanFile("level1/level1_" + i + ".in");
+            solve(scanner, i);
+        }
     }
-    
-    static void solve(Scanner scanner) {
+
+    static void solve(Scanner scanner, int i) {
         Invader invader = new Invader(scanner.nextInt(), scanner.nextInt());
-//        scanner.nextLine()
+        while (scanner.hasNext()) {
+            String command = scanner.next();
+            int times = scanner.nextInt();
+            switch (command) {
+                case "T":
+                    invader.turn(times);
+                    break;
+                case "F":
+                    invader.move(times);
+                    break;
+            }
+        }
+        IOUtils.write("level1/level1_" + i + ".out",invader.x + " " + invader.y);
+        System.out.println(invader.x + " " + invader.y);
     }
     
 }
